1b027bc023365878f2c7f35b3a32d8d1694d59e8
johan bustos <jsbustose@udistrital.edu.co>
2024-03-20 15:40:24 -0500

    tallerclase

---
+++ dado.py
@@ -0,0 +1,2 @@
+import random
+numero= random
--- pyhones.py
+++ pyhones.py
@@ -35,3 +35,38 @@ a=True
 b=False
 
 print(a and b)
+
+#los operadores booleanos y de comparacion son muy utilizados al
+# definir condiciones
+
+#sentencias de control de codigo: En general un programa se ejecuta
+# Linea por linea de manera secuancial. Se puede romper esa secuencialidad
+# empleado un conjunto de sentencias (expresiones) que permite:
+# 1. seleccionar la ejecucion de un bloque de codigo
+# 2. Reptir la ejecucion de uin bloque de codigo
+# 3. seleccionar entre ejecutar un bloque de codigo u otro bloque de codigo
+# De esa manera podemos "romper" la secuencialidad
+# Principos de paradigma de programacion estructurado
+
+# sentencia if. si se cumple una condicion (se evalua como true)
+# se ejecuta un bloque de codigo
+
+print("linea 1")
+print("linea 2")
+if 5>8 or 3<7:
+    print("Esto se muetra si la condicion es verdadera")
+    
+else:
+    print("Esto se muetra si la condicion es false")
+    
+entrada= int (input("cuantos años tiene?"))
+
+if entrada< 18:
+    print("Es un menor de edad")
+    
+else: 
+    print("ya no esta disponible para su edad")
+    
+#taller crear un programa en python que genere un numero aleatroio 
+#Entre 2 y 12. si el numero es 7 imprimir gano. si no imprimir perdio 
+
