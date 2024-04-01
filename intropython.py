c5fff3cf94e526af133861bd8293a17d6c7f35aa
johan bustos <jsbustose@udistrital.edu.co>
2024-04-01 14:54:31 -0500

    ciclos

---
+++ intropython.py
@@ -0,0 +1,15 @@
+# while
+# while <condicon_verdadera>:
+#      cuerpo del ciclo
+#condiciones son: expresiones booleanas (or, and) y operaciones de comparación
+# ciclos controlados por un contador enteros
+
+i=0
+k=0
+
+while 1==1:
+   print("ciclo")
+    #importante modificar el valor del contador 
+    i+1
+    
+# for
